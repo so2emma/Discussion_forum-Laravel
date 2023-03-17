56d5a60aa55b036fafea86777567adeeb7a49b1a
Oso Emmanuel <osoemmanuel1969@gmail.com>
2023-03-17 16:30:53 +0100

    implementing Queue jobs

--- app/Notifications/NewReplyAdded.php
+++ app/Notifications/NewReplyAdded.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class NewReplyAdded extends Notification
+class NewReplyAdded extends Notification implements ShouldQueue
 {
     use Queueable;
 
